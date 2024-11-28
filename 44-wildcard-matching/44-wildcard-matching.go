dc3c1b16e61f6b59772c0bdab2e0ba31d265acfa
grbll@posteo.com <grbll@posteo.com>
2024-11-28 17:14:30 +0100

    new approach for 44

--- 44-wildcard-matching/44-wildcard-matching.go
+++ 44-wildcard-matching/44-wildcard-matching.go
@@ -1,92 +1,140 @@
 package wildcardmatching
 
-// import "slices"
-//
-// type store struct {
-// 	jumps []int
-// }
-//
-// func buildMapper(p string) *store {
-// 	var n int = len(p)
-// 	var pStore *store = &store{}
-// 	var count int
-//
-// 	for i := 0; i < n; i++ {
-// 		if p[i] != '*' {
-// 			count++
-// 		}
-// 	}
-//
-// 	pStore.jumps = make([]int, count+1, count+1)
-// 	pStore.jumps[count] = n
-// 	count = 0
-//
-// 	for i := 0; i < n; i++ {
-// 		if p[i] != '*' {
-// 			pStore.jumps[count] = i
-// 			count++
-// 		}
-// 	}
-// 	return pStore
-// }
-//
-// func (pStore *store) jump(pPositions []bool) {
-// 	var n = len(pPositions)
-// 	var index, next int
-// 	index = 0
-// 	for index < n {
-// 		if pPositions[index] {
-// 			next, _ = slices.BinarySearch(pStore.jumps, index)
-// 			for index <= pStore.jumps[next] {
-// 				pPositions[index] = true
-// 				index++
-// 			}
-// 		} else {
-// 			index++
-// 		}
-// 	}
-// }
+func merge(ranges, cutter, newRanges []int) []int {
+	newRanges = []int{}
+	var indexRanges, indexCutter = 0, 0
+	var upper, lower *[]int
+	var indexLower, indexUpper *int
 
-func isMatch(s string, p string) bool {
-	var n = len(p)
-
-	var positions []bool
-
-	positions = make([]bool, n+1, n+1)
-	positions[0] = true
-
-	for index := 0; index < n; index++ {
-		if p[index] != '*' {
-			break
+	for indexRanges < len(ranges) && indexCutter < len(cutter) {
+		if ranges[indexRanges+1]-1 < cutter[indexCutter] {
+			indexRanges += 2
+		} else if cutter[indexCutter+1]-1 < ranges[indexRanges] {
+			indexCutter++
 		} else {
-			positions[index+1] = true
+			if ranges[indexRanges] < cutter[indexCutter] {
+				indexLower = &indexCutter
+				lower = &cutter
+			} else {
+				indexLower = &indexRanges
+				lower = &ranges
+			}
+			if ranges[indexRanges+1] < cutter[indexRanges+1] {
+				indexUpper = &indexRanges
+				upper = &ranges
+			} else {
+				indexUpper = &indexCutter
+				upper = &cutter
+			}
+			newRanges = append(newRanges, (*lower)[*indexLower]+1, (*upper)[(*indexUpper)+1]+1)
+			*indexUpper += 2
 		}
 	}
 
-	for _, letter := range []byte(s) {
-		positions[n] = false
-		for index := n; index >= 0; index-- {
-			if positions[index] {
-				switch p[index] {
-				case '*':
-					continue
-				case '?':
-					positions[index] = false
-					positions[index+1] = true
-				case letter:
-					positions[index] = false
-					positions[index+1] = true
-				default:
-					positions[index] = false
-				}
-			}
-		}
-		for index := 0; index < n; index++ {
-			if positions[index] && p[index] == '*' {
-				positions[index+1] = true
-			}
-		}
-	}
-
-	return positions[n]
+	return newRanges
 }
+
+// func isMatch(s string, p string) bool { // new approach, marking s instead of p. old approachl below.
+// 	var n int = len(s)
+//
+// 	var ranges, buildRanges []int = make([]int, 0, n+2), make([]int, 0, n+2)
+// 	var cutterMap map[byte][]int
+// 	var cutter []int
+// 	var rangeIndex int
+//
+// 	ranges = []int{0, 1}
+// 	for _, symbol := range []byte(p) {
+// 		if symbol == '*' {
+// 			ranges = []int{ranges[0], n + 1}
+// 		} else {
+//
+// 			cutter, exists := cutterMap[symbol]
+// 			if !exists {
+// 				cutterMap[symbol] = make([]int, 0, n)
+// 			}
+//
+// 			buildRanges = []int{}
+// 			rangeIndex = 0
+// 			for cutterIndex, boarder := range cutter {
+// 				for boarder <
+// 			}
+// 		}
+// 		if len(ranges) == 0 {
+// 			return false
+// 		}
+// 	}
+// 	return true
+// }
+
+// var mustConsume, canConsume int = 0, 0
+// for _, symbol := range []byte(p) {
+// 	if symbol == '*' {
+// 		canConsume = n
+// 	} else {
+// 		mustConsume++
+// 		canConsume = min(canConsume+1, n)
+// 		if symbol != '?' {
+// 			for canConsume >= mustConsume {
+// 				if s[canConsume-1] == symbol {
+// 					break
+// 				} else {
+// 					canConsume--
+// 				}
+// 			}
+// 			for mustConsume <= canConsume {
+// 				if s[mustConsume-1] == symbol {
+// 					break
+// 				} else {
+// 					mustConsume++
+// 				}
+// 			}
+// 		}
+// 	}
+// 	if mustConsume > canConsume {
+// 		return false
+// 	}
+// }
+
+// func isMatch(s string, p string) bool {
+// 	var n = len(p)
+//
+// 	var positions []bool
+//
+// 	positions = make([]bool, n+1, n+1)
+// 	positions[0] = true
+//
+// 	for index := 0; index < n; index++ {
+// 		if p[index] != '*' {
+// 			break
+// 		} else {
+// 			positions[index+1] = true
+// 		}
+// 	}
+//
+// 	for _, letter := range []byte(s) {
+// 		positions[n] = false
+// 		for index := n; index > 0; index-- {
+// 			if positions[index-1] {
+// 				switch p[index-1] {
+// 				case '*':
+// 					continue
+// 				case '?':
+// 					positions[index-1] = false
+// 					positions[index] = true
+// 				case letter:
+// 					positions[index-1] = false
+// 					positions[index] = true
+// 				default:
+// 					positions[index-1] = false
+// 				}
+// 			}
+// 		}
+// 		for index := 0; index < n; index++ {
+// 			if positions[index] && p[index] == '*' {
+// 				positions[index+1] = true
+// 			}
+// 		}
+// 	}
+//
+// 	return positions[n]
+// }
--- 44-wildcard-matching/44-wildcard-matching_test.go
+++ 44-wildcard-matching/44-wildcard-matching_test.go
@@ -2,45 +2,115 @@ package wildcardmatching
 
 import (
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
 	// "github.com/google/go-cmp/cmp"
 )
 
-func Test_isMatch(t *testing.T) {
+func Test_merge(t *testing.T) {
 	type args struct {
-		s string
-		p string
+		ranges    []int
+		cutter    []int
+		newRanges []int
 	}
 	tests := []struct {
 		name string
 		args args
-		want bool
+		want []int
 	}{
 		{
-			name: "string empty, pattern star",
-			args: args{s: "", p: "**"},
-			want: true,
+			name: "two identical ranges",
+			args: args{ranges: []int{0, 2}, cutter: []int{0, 2}, newRanges: []int{}},
+			want: []int{1, 3},
 		},
 		{
-			name: "simple match",
-			args: args{s: "aa", p: "aa"},
-			want: true,
+			name: "intersection",
+			args: args{ranges: []int{0, 3}, cutter: []int{1, 4}, newRanges: []int{}},
+			want: []int{2, 4},
 		},
 		{
-			name: "first simple non match",
-			args: args{s: "aa", p: "a"},
-			want: false,
+			name: "intersection other way",
+			args: args{ranges: []int{1, 4}, cutter: []int{0, 3}, newRanges: []int{}},
+			want: []int{2, 4},
 		},
 		{
-			name: "complete match by *",
-			args: args{s: "aa", p: "*"},
-			want: true,
+			name: "inclusion",
+			args: args{ranges: []int{1, 5}, cutter: []int{2, 3}, newRanges: []int{}},
+			want: []int{3, 4},
+		},
+		{
+			name: "empty",
+			args: args{ranges: []int{1, 5}, cutter: []int{5, 6}, newRanges: []int{}},
+			want: []int{},
+		},
+		{
+			name: "inclusion, plus irrelevant",
+			args: args{ranges: []int{1, 5}, cutter: []int{2, 4, 5, 6}, newRanges: []int{}},
+			want: []int{3, 5},
+		},
+		{
+			name: "complex",
+			args: args{ranges: []int{1, 7, 8, 10}, cutter: []int{2, 4, 5, 8}, newRanges: []int{}},
+			want: []int{3, 5, 6, 9},
 		},
 	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := isMatch(tt.args.s, tt.args.p); got != tt.want {
+			got := merge(tt.args.ranges, tt.args.cutter, tt.args.newRanges)
+			if !cmp.Equal(got, tt.want) {
 				t.Errorf("isMatch(%+v) = %v, want %v", tt.args, got, tt.want)
 			}
 		})
 	}
 }
+
+// func Test_isMatch(t *testing.T) {
+// 	type args struct {
+// 		s string
+// 		p string
+// 	}
+// 	tests := []struct {
+// 		name string
+// 		args args
+// 		want bool
+// 	}{
+// 		{
+// 			name: "mixed case, with wildcard",
+// 			args: args{s: "acdcb", p: "a*c?b"},
+// 			want: false,
+// 		},
+// 		{
+// 			name: "mixed case",
+// 			args: args{s: "adceb", p: "*a*b"},
+// 			want: true,
+// 		},
+// 		{
+// 			name: "string empty, pattern star",
+// 			args: args{s: "", p: "**"},
+// 			want: true,
+// 		},
+// 		{
+// 			name: "simple match",
+// 			args: args{s: "aa", p: "aa"},
+// 			want: true,
+// 		},
+// 		{
+// 			name: "first simple non match",
+// 			args: args{s: "aa", p: "a"},
+// 			want: false,
+// 		},
+// 		{
+// 			name: "complete match by *",
+// 			args: args{s: "aa", p: "*"},
+// 			want: true,
+// 		},
+// 	}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			if got := isMatch(tt.args.s, tt.args.p); got != tt.want {
+// 				t.Errorf("isMatch(%+v) = %v, want %v", tt.args, got, tt.want)
+// 			}
+// 		})
+// 	}
+// }
