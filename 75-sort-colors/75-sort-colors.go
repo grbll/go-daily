23c8b84cd19a0f8dc73b3321fdd004a1d5832328
grbll@posteo.com <grbll@posteo.com>
2025-01-23 12:11:09 +0100

    added new approach to 75 with slightly better runtime.

--- 75-sort-colors/75-sort-colors.go
+++ 75-sort-colors/75-sort-colors.go
@@ -1,21 +1,45 @@
 package sortcolors
 
 func sortColors(nums []int) {
-	var zeroCount, oneCount int = 0, 0
-	for i := 0; i < len(nums); i++ {
-		if nums[i] == 0 {
-			zeroCount++
-		} else if nums[i] == 1 {
-			oneCount++
+	var bot, mid, top = -1, 0, len(nums)
+	for mid < top && nums[mid] != 1 {
+		if nums[mid] == 0 {
+			bot++
+			mid++
+		} else {
+			top--
+			nums[mid], nums[top] = nums[top], nums[mid]
 		}
 	}
-	for i := 0; i < zeroCount; i++ {
-		nums[i] = 0
-	}
-	for i := zeroCount; i < zeroCount+oneCount; i++ {
-		nums[i] = 1
-	}
-	for i := zeroCount + oneCount; i < len(nums); i++ {
-		nums[i] = 2
+	for mid < top {
+		if nums[mid] == 0 {
+			bot++
+			nums[bot], nums[mid] = nums[mid], nums[bot]
+		} else if nums[mid] == 2 {
+			top--
+			nums[top], nums[mid] = nums[mid], nums[top]
+		} else {
+			mid++
+		}
 	}
 }
+
+// func sortColors(nums []int) {
+// 	var zeroCount, oneCount int = 0, 0
+// 	for i := 0; i < len(nums); i++ {
+// 		if nums[i] == 0 {
+// 			zeroCount++
+// 		} else if nums[i] == 1 {
+// 			oneCount++
+// 		}
+// 	}
+// 	for i := 0; i < zeroCount; i++ {
+// 		nums[i] = 0
+// 	}
+// 	for i := zeroCount; i < zeroCount+oneCount; i++ {
+// 		nums[i] = 1
+// 	}
+// 	for i := zeroCount + oneCount; i < len(nums); i++ {
+// 		nums[i] = 2
+// 	}
+// }
