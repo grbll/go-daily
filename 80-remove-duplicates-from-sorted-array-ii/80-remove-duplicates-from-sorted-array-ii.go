753eebcdbcd4d353ff37b23c815262c0797714ff
grbll@posteo.com <grbll@posteo.com>
2025-01-30 19:31:54 +0100

    added 80. not finished. practiced: heap search.

---
+++ 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.go
@@ -0,0 +1,47 @@
+package removeduplicatesfromsortedarrayii
+
+func rise(nums []int, index int) {
+	for nums[index] < nums[(len(nums)-1)-(len(nums)-1-index)/2] {
+		nums[index], nums[(len(nums)-1)-(len(nums)-1-index)/2] = nums[(len(nums)-1)-(len(nums)-1-index)/2], nums[index]
+		index = (len(nums) - 1) - (len(nums)-1-index)/2
+	}
+}
+
+func sink(nums []int, end int) {
+	var index, double int = len(nums) - 1, len(nums) - 1
+	for {
+		if double-2 >= end && nums[index] > nums[double-2] && nums[double-2] < nums[double-1] {
+			nums[index], nums[double-2] = nums[double-2], nums[index]
+			index = double - 2
+		} else if double-1 >= end && nums[index] > nums[double-1] {
+			nums[index], nums[double-1] = nums[double-1], nums[index]
+			index = double - 1
+		} else {
+			break
+		}
+		double = len(nums) - 1 - (len(nums)-1-index)*2
+	}
+}
+
+func removeDuplicates(nums []int) int {
+	if len(nums) < 3 {
+		return len(nums)
+	}
+	for i := len(nums) - 1; i >= 0; i-- {
+		rise(nums, i)
+	}
+	nums[len(nums)-1], nums[0] = nums[0], nums[len(nums)-1]
+	sink(nums, 1)
+	nums[len(nums)-1], nums[1] = nums[1], nums[len(nums)-1]
+	sink(nums, 2)
+	var end, k int = 2, 2
+	for end < len(nums) {
+		end++
+		nums[k], nums[len(nums)-1] = nums[len(nums)-1], nums[end-1]
+		sink(nums, end)
+		if nums[k] != nums[k-2] {
+			k++
+		}
+	}
+	return k
+}
